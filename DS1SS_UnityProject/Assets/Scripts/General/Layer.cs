a7249b4265af2f00bdcef53a736fb5db468791e0
Meemitone <99894591+Meemitone@users.noreply.github.com>
2023-05-16 17:37:36 +0100

    First Layer Prototype

---
+++ DS1SS_UnityProject/Assets/Scripts/General/Layer.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Layer : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onBecomeActive;
+    [SerializeField] private UnityEvent onBecomeInactive;
+    [SerializeField] private UnityEvent onBecomeHidden;
+    [SerializeField] private float currentScale = 1;
+    [SerializeField] private float currentAlpha = 1;
+    public bool runningCR { get; private set; } = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move the layer to track to the camera
+    }
+
+    internal void SetInactiveAndHide(int dir)
+    {
+        if (runningCR)
+        {
+            return;
+        }
+        if (dir < 0)
+            StartCoroutine(Transition(gameObject.transform.parent.GetComponent<LayerManager>().transitionLength, currentScale * gameObject.transform.parent.GetComponent<LayerManager>().relativeLayerScale, 1f));
+        if (dir > 0)
+            StartCoroutine(Transition(gameObject.transform.parent.GetComponent<LayerManager>().transitionLength, currentScale / gameObject.transform.parent.GetComponent<LayerManager>().relativeLayerScale, 1f));
+        onBecomeHidden.Invoke();
+    }
+
+    internal void SetActive(int dir)
+    {
+        if (runningCR)
+        {
+            return;
+        }
+        if (dir < 0)
+            StartCoroutine(Transition(gameObject.transform.parent.GetComponent<LayerManager>().transitionLength, currentScale * gameObject.transform.parent.GetComponent<LayerManager>().relativeLayerScale, 1f));
+        if (dir > 0)
+            StartCoroutine(Transition(gameObject.transform.parent.GetComponent<LayerManager>().transitionLength, currentScale / gameObject.transform.parent.GetComponent<LayerManager>().relativeLayerScale, 1f));
+        onBecomeActive.Invoke();
+    }
+
+    internal void SetInactiveAndBehind(int dir)
+    {
+        if (runningCR)
+        {
+            return;
+        }
+        if (dir < 0)
+            StartCoroutine(Transition(gameObject.transform.parent.GetComponent<LayerManager>().transitionLength, currentScale * gameObject.transform.parent.GetComponent<LayerManager>().relativeLayerScale, 0f));
+        if (dir > 0)
+            StartCoroutine(Transition(gameObject.transform.parent.GetComponent<LayerManager>().transitionLength, currentScale / gameObject.transform.parent.GetComponent<LayerManager>().relativeLayerScale, 0f));
+        onBecomeInactive.Invoke();
+    }
+
+
+    [Tooltip("enable or disable all Colliders in the layer")]
+    public void CollidersToggle(bool mode)
+    {
+        Collider2D[] all = GetComponentsInChildren<Collider2D>();
+        foreach (Collider2D c in all)
+        {
+            c.enabled = mode;
+        }
+    }
+
+    public IEnumerator Transition(float t, float s, float a)
+    {
+        runningCR = true;
+        float timePassed = 0f;
+        float initScale = currentScale;
+        float initAlpha = currentAlpha;
+        SpriteRenderer[] rends = GetComponentsInChildren<SpriteRenderer>();
+        while (timePassed < t)
+        {
+            currentScale = Mathf.Lerp(initScale, s, timePassed / t);
+            currentAlpha = Mathf.Lerp(initAlpha, a, timePassed / t);
+            gameObject.transform.localScale = Vector3.one * currentScale;
+            foreach (SpriteRenderer r in rends)
+            {
+                r.color = new Color(r.color.r, r.color.g, r.color.b, currentAlpha);
+            }
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+        currentScale = s;
+        currentAlpha = a;
+        gameObject.transform.localScale = Vector3.one * currentScale;
+        foreach (SpriteRenderer r in rends)
+        {
+            r.color = new Color(r.color.r, r.color.g, r.color.b, currentAlpha);
+        }
+        runningCR = false;
+        yield return null;
+    }
+}
---
+++ DS1SS_UnityProject/Assets/Scripts/General/LayerManager.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LayerManager : MonoBehaviour
+{
+    [SerializeField] private List<Layer> layers;
+    [Tooltip("Set this to the initial starting layer or perish")]
+    [SerializeField] private int activeLayer = -1;
+    public UnityEvent OnLayerSwitch;
+    [Tooltip("Size the layer shrinks to when moved back")]
+    [SerializeField] public float relativeLayerScale { get; private set; } = 0.5f;
+    [SerializeField] public float transitionLength { get; private set; } = 0.5f;
+
+    //Awake is called before start
+    private void Awake()
+    {
+        //Start Section "Replace when load save" (move this idea to the start of start so load can be in awake)
+        if (activeLayer < 0 || activeLayer >= layers.Count)
+        {
+            Debug.LogWarning("Warning: LayerManager appears to be set up incorrectly, layer set to frontmost as a default");
+            activeLayer = 0;
+            if (layers.Count == 0)
+            {
+                Debug.LogError("Error: No Layers set up. Forcing stop.");
+
+#if UNITY_EDITOR
+                EditorApplication.isPlaying = false;//unplays your editor
+#endif
+
+                Application.Quit();
+            }
+        }
+        //End Section "Replace when load save"
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < layers.Count; i++)
+        {
+            int dif = activeLayer - i;
+            StartCoroutine(layers[i].Transition(0.001f, Mathf.Pow(relativeLayerScale, dif), dif < 0 ? 0f : 1f));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!layers[0].runningCR)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                ChangeLayer(activeLayer - 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                ChangeLayer(activeLayer + 1);
+            }
+        }
+    }
+
+    [Tooltip("Change the 'active' group of objects")]
+    public void ChangeLayer([Tooltip("negative is to the cam")] int target)
+    {
+        int direction = activeLayer - target;
+        if (target < 0 || target >= layers.Count)
+        {
+            return;
+        }
+        for (int i = 0; i < layers.Count; i++)
+        {
+            if (target > i)
+            {
+                layers[i].SetInactiveAndHide(direction);
+            }
+            else if (target == i)
+            {
+                layers[i].SetActive(direction);
+            }
+            else if (target < i)
+            {
+                layers[i].SetInactiveAndBehind(direction);
+            }
+            else
+            {
+                //PANIC
+            }
+        }
+        activeLayer = target;
+        OnLayerSwitch.Invoke();
+    }
+
+    [Tooltip("Change the 'active' group of objects to the frontmost")]
+    public void ChangeLayer([Tooltip("Name of layer GameObject")] string target)
+    {
+        for (int i = 0; i < layers.Count; i++)
+        {
+            if (layers[i].name == target)
+            {
+                ChangeLayer(i);
+                return;
+            }
+        }
+    }
+
+
+    //Get the Layer that a gameObject is on by recursively checking parents
+    public int GetLayerFromObject(GameObject target)
+    {
+        Transform thing = target.transform;
+        Layer answer;
+        while (thing != null)
+        {
+            if (target.TryGetComponent<Layer>(out answer))
+            {
+                return layers.FindIndex(i => i.Equals(answer));
+            }
+            else
+            {
+                thing = thing.parent;
+            }
+        }
+        Debug.LogWarning("LayerNotFound");
+        return -1;
+    }
+
+}
