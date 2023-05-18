e88f73330ddbece926903fb695d883b82947e204
jacobbb61 <mcconnelljacob61@gmail.com>
2023-05-18 14:51:36 +0100

    Main Menu UI

---
+++ DS1SS_UnityProject/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Start Screen")]
+
+    public GameObject PressStartButton;
+    public bool PressedStart;
+
+
+}
