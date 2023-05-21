d790f9a1918bfe7d5aff924801889bb6193730ce
jacobbb61 <mcconnelljacob61@gmail.com>
2023-05-21 18:43:07 +0100

    More Main Menu Setup

--- DS1SS_UnityProject/Assets/Scripts/UI/MainMenuManager.cs
+++ DS1SS_UnityProject/Assets/Scripts/UI/MainMenuManager.cs
@@ -8,22 +8,54 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    private bool CanInput = true;
+    public bool CanInput = true;
 
     public Animator SceneTransitionAnim;
-    public RectTransform HightlightPos;
-    public Animator HightlightAnim;
-
-    public int MainOrder = 1;
     public string ActiveMenu = "Main";
 
+    [Header("Main")]
+    public RectTransform MainHightlightPos;
+    public Animator MainHightlightAnim; 
+    public int MainOrder = 1;
+
+    [Header("New Game Message")]
+    public GameObject NewGameMessage;
+    public RectTransform NGHightlightPos;
+    public Animator NGHightlightAnim;
+    public bool NGOrder;
+
+    [Header("Load Game")]
+    public GameObject LoadGameText;
+    public GameObject LoadGameTextGrey;
+    public bool HasGameSave;
+
+    private void Start()
+    {
+        ActiveMenu = "Main";
+        MainOrder = 1;
+        MoveMainHighlight();
+
+        NewGameMessage.SetActive(false);
+        NGOrder = false;
+        MoveNGHighlight();
+
+        LoadGameText.SetActive(HasGameSave);
+        LoadGameTextGrey.SetActive(!HasGameSave);
+    }
     public void Up(InputAction.CallbackContext context)
     {
         if (context.action.triggered && CanInput == true)
         {
             CanInput = false;
-            MainOrder--;
-            MoveMainHighlight();
+            Debug.Log("Up Button Pressed");
+            switch (ActiveMenu)
+            {
+                case "Main":
+                    MainOrder--;
+                    MoveMainHighlight();
+                    break;
+           
+            }
         }
     }
     public void Down(InputAction.CallbackContext context)
@@ -31,62 +63,108 @@ public class MainMenuManager : MonoBehaviour
         if (context.action.triggered && CanInput == true)
         {
             CanInput = false;
-            MainOrder++;
-            MoveMainHighlight();
+            Debug.Log("Down Button Pressed");
+            switch (ActiveMenu)
+            {
+                case "Main":
+                    MainOrder++;
+                    MoveMainHighlight();
+                    break;
+                default:
+                    CanInput = true;
+                    break;
+            }
         }
     }
+    public void Left(InputAction.CallbackContext context)
+    {
+        if (context.action.triggered && CanInput == true)
+        {
+            CanInput = false;
+            Debug.Log("Left Button Pressed");
+            switch (ActiveMenu)
+            {
+                case "New Game Message":
+                    NGOrder = true;
+                    MoveNGHighlight();                  
+                    break;
+            }
+        }
+    }
+    public void Right(InputAction.CallbackContext context)
+    {
+        if (context.action.triggered && CanInput == true)
+        {
+            CanInput = false;
+            Debug.Log("Right Button Pressed");
+            switch (ActiveMenu)
+            {
+                case "New Game Message":
+                    NGOrder = false;
+                    MoveNGHighlight();
+                    CanInput = true;
+                    break;
+            }
+        }
+    }
+
     public void A(InputAction.CallbackContext context)
     {
         if (context.action.triggered && CanInput == true)
         {
-            Debug.Log("Button Pressed");
+            Debug.Log("A Button Pressed");
             CanInput = false;
-            if (ActiveMenu == "Main") { SelectMain(); }
+            switch (ActiveMenu)
+            { 
+                
+               case "New Game Message":
+                    SelectNG();
+                    break;
+
+                case "Main":
+                    SelectMain();                   
+                    break;
+
+               
+            }
         }
     }
-
-    public void MoveMainHighlight()
+    public void B(InputAction.CallbackContext context)
     {
-        switch (MainOrder)
+        if (context.action.triggered && CanInput == true)
         {
-            case 0:
-                MainOrder = 5; MoveMainHighlight();
-                break;
-
-            case 1: 
-                HightlightPos.anchoredPosition = new Vector2(0, -50);
-                break;
-
-            case 2:
-                HightlightPos.anchoredPosition = new Vector2(0, -80);
-                break;
-
-            case 3:
-                HightlightPos.anchoredPosition = new Vector2(0, -110);
-                break;
-
-            case 4:
-                HightlightPos.anchoredPosition = new Vector2(0, -140);
-                break;
-
-            case 5:
-                HightlightPos.anchoredPosition = new Vector2(0, -170);
-                break;
-
-            case 6:
-                MainOrder = 1; MoveMainHighlight();
-                break;
+            Debug.Log("B Button Pressed");
+            CanInput = false;
+            switch (ActiveMenu)
+            {
+                case "New Game Message":
+                    ActiveMenu = "Main";
+                    NewGameMessage.SetActive(false);
+                    NGOrder = false;
+                    CanInput = true;
+                    break;
+            }
         }
-        CanInput = true;
     }
+
+
+
 
     public void SelectMain()
     {
-        HightlightAnim.SetTrigger("Active");
+        MainHightlightAnim.SetTrigger("Active");
         switch (MainOrder)
         {
             case 1:
-                CanInput = true;
+                if (HasGameSave)
+                {
+                    NewGameMessage.SetActive(true);
+                    NGOrder = false;
+                    MoveNGHighlight();
+                    CanInput = true;
+                    ActiveMenu = "New Game Message";
+                }
+                else { StartCoroutine(LoadNewGame()); }
                 break;
 
             case 2:
@@ -107,7 +185,87 @@ public class MainMenuManager : MonoBehaviour
         }
        
     }
+    public void SelectNG()
+    {
+        NGHightlightAnim.SetTrigger("Active");
+        if (NGOrder)
+        {
+            StartCoroutine(LoadNewGame());
+        }
+        else
+        {
+            StartCoroutine(ExitNewGame());      
+        }
+    }
 
+
+
+    public void MoveMainHighlight()
+    {
+        switch (MainOrder)
+        {
+            case 0:
+                MainOrder = 5; MoveMainHighlight();
+                break;
+
+            case 1: 
+                MainHightlightPos.anchoredPosition = new Vector2(0, -50);
+                break;
+
+            case 2:
+                MainHightlightPos.anchoredPosition = new Vector2(0, -80);
+                break;
+
+            case 3:
+                MainHightlightPos.anchoredPosition = new Vector2(0, -110);
+                break;
+
+            case 4:
+                MainHightlightPos.anchoredPosition = new Vector2(0, -140);
+                break;
+
+            case 5:
+                MainHightlightPos.anchoredPosition = new Vector2(0, -170);
+                break;
+
+            case 6:
+                MainOrder = 1; MoveMainHighlight();
+                break;
+        }
+        CanInput = true;
+    }
+    public void MoveNGHighlight()
+    {
+
+        if (NGOrder) 
+        {
+            NGHightlightPos.anchoredPosition = new Vector2(-80, -30);
+        } 
+        else
+        {
+            NGHightlightPos.anchoredPosition = new Vector2(80, -30);
+        }
+
+        CanInput = true;
+    }
+
+
+    IEnumerator LoadNewGame()
+    {
+        Debug.Log("Loading New Game");
+        SceneTransitionAnim.SetTrigger("Active");
+        yield return new WaitForSeconds(1.5f);
+        //SceneManager.LoadScene("End Credits", LoadSceneMode.Single); Load new game scene
+        SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+    }
+    IEnumerator ExitNewGame()
+    {     
+        yield return new WaitForSeconds(0.25f);
+        ActiveMenu = "Main";
+        NewGameMessage.SetActive(false);
+        NGOrder = false;
+        CanInput = true;
+    }
     IEnumerator LoadCredits()
     {
         Debug.Log("Loading End Credits");
