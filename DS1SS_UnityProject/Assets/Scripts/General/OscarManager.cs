7662b58ea99f93f0c9f8533579f1052621cb223f
jacobbb61 <mcconnelljacob61@gmail.com>
2023-05-24 14:23:41 +0100

    Dialogue system working with text and audio

--- DS1SS_UnityProject/Assets/Scripts/General/OscarManager.cs
+++ DS1SS_UnityProject/Assets/Scripts/General/OscarManager.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using TMPro;
-
+using FMODUnity;
 public class OscarManager : MonoBehaviour
 {
     public bool CanInput = true;
@@ -21,6 +22,7 @@ public class OscarManager : MonoBehaviour
     public int CurrentTextLine=0;
     public bool IsTalking = false;
 
+
     [Header("Question")]
     public GameObject QuestionObject;
     public RectTransform QuestionHightlightPos;
@@ -33,15 +35,29 @@ public class OscarManager : MonoBehaviour
     public List<string> StateATextLines = new List<string>();
     public List<string> StateYESTextLines = new List<string>();
     public List<string> StateNOTextLines = new List<string>();
+    public List<string> StateBTextLines = new List<string>();
+    public List<string> StateCTextLines = new List<string>();
+    public List<string> StateDFTextLines = new List<string>();
+    public List<string> StateGTextLines = new List<string>();
+    public List<string> StateHTextLines = new List<string>();
+    public List<string> StateITextLines = new List<string>();
 
-    [Header("Audio clips")]
-    public List<AudioClip> StateAAudioClips = new List<AudioClip>();
+    [Header("Audio Events")]
+    private FMOD.Studio.EventInstance instance;
+    private Coroutine RoutineToStop;
+    public List<EventReference> StateAAudioClips = new List<EventReference>();
 
 
     public void Start()
     {
         QuestionOpen = false;
         QuestionObject.SetActive(false);
+
+        DialogObject.SetActive(false);
+        IsTalking = false;
+
+        CurrentTextLine = 0;
+        CurrentText = "You should not be able to see this";
 
         StateATextLines.Add("...Oh, you... You're no Hollow, eh? Thank goodness...");
         StateATextLines.Add("...That boulder was you?...HA...I thank you for returning the favour...");
@@ -61,6 +77,30 @@ public class OscarManager : MonoBehaviour
 
         StateNOTextLines.Add("… Yes, I see… Perhaps I was too hopeful… Hah hah…");
         StateNOTextLines.Add("…May our paths cross once more… Farewell…");
+
+        StateBTextLines.Add("...Perhaps you have more luck courting than slaying…");
+        StateBTextLines.Add("…Ha ha I jest.. Prithee forgive my brash comment…");
+        StateBTextLines.Add("...hmm… there must be another way…");
+
+        StateCTextLines.Add("...Uhh, ahh, a knight… Unlike any I’ve seen before…");
+        StateCTextLines.Add("…take these… m-my estus flasks… you’ll need them…");
+
+        StateDFTextLines.Add("...Hrggkt... But... Why…");
+
+        StateGTextLines.Add("...Ah… still determined I see… you’re built of sturdier stuff than I …hmm… ");
+        StateGTextLines.Add("…Best to spread our efforts… I’ll search the asylum for other exits while you fight…");
+        StateGTextLines.Add("…I wish you luck in your… battles…");
+        StateGTextLines.Add("...Hmm… perhaps the balcony…");
+
+        StateHTextLines.Add("...Ah… shame you had to find me in such a state… my legs…");
+        StateHTextLines.Add("…such a fool…I-I wish to ask one last thing of you… ");
+        StateHTextLines.Add("…tell them… tell them I- … s-stay here w-with me for but a moment, please…");
+        StateHTextLines.Add("…M-may the flames.. guide thee...");
+
+        StateITextLines.Add("...Ah, there you are… I see you’ve felled that wretched beast... ");
+        StateITextLines.Add("…Gave me quite a bit of trouble on the rooftops…");
+        StateITextLines.Add("…You go on ahead… I shall catch up with you in due time…");
+
     }
 
     public void Y(InputAction.CallbackContext context)
@@ -71,8 +111,8 @@ public class OscarManager : MonoBehaviour
             CanInput = false;
 
             if (!IsTalking) { OpenDialog(); }
-
-            NextLine();           
+            NextLine();
+           // StopAnyAudio();//stop audio and timer     
         }
     }
     public void A(InputAction.CallbackContext context)
@@ -118,38 +158,75 @@ public class OscarManager : MonoBehaviour
 
     public void NextLine()
     {
-        
-        switch (CurrentState)
+        if (IsTalking) { StopAnyAudio(); } //stop previous audio to play/skip to next
+        IsTalking = true; //currently talking
+        switch (CurrentState) //set dialog order a
         {           
             case "A":
-                if (CurrentTextLine<4) { CurrentText = StateATextLines[CurrentTextLine]; CurrentTextLine++; } else { CurrentText = StateATextLines[3]; OpenQuestion(); } //show new text
-                //play audio of current line 
-                //start timer based on length of audio clip of current line to change text
+                if (CurrentTextLine<3) {
+                    PlayAudio(StateAAudioClips[CurrentTextLine].Path);
+                    RoutineToStop = StartCoroutine(WaitForAudioToEnd());//start audio timer            
+                    CurrentText = StateATextLines[CurrentTextLine]; //tell dialogue text what to show
+                    CurrentTextLine++;
+                } 
+                else {
+                    PlayAudio(StateAAudioClips[CurrentTextLine].Path);
+                    instance.start(); //play audio);//play audio  
+                    CurrentTextLine = 3; // repeat the 4th line of dialog text
+                    CurrentText = StateATextLines[3]; //tell dialogue text what to show
+                    IsTalking = false; //done talking
+                    OpenQuestion();
+                } 
+
                 break;
             case "YES":
-                if (CurrentTextLine < 9) { CurrentText = StateYESTextLines[CurrentTextLine]; CurrentTextLine++; } else { CurrentText = StateYESTextLines[9]; }
+                if (CurrentTextLine < 10) { PlayAudio(StateAAudioClips[CurrentTextLine].Path); CurrentText = StateYESTextLines[CurrentTextLine]; CurrentTextLine++; } 
+                else { CurrentText = StateYESTextLines[9]; CurrentTextLine = 9; CloseDialog(); }
                 break;
             case "NO":
-                if (CurrentTextLine < 2) { CurrentText = StateNOTextLines[CurrentTextLine]; CurrentTextLine++; } else { CurrentState = "A"; CurrentTextLine = 4; CurrentText = StateATextLines[3];  }
+                if (CurrentTextLine < 2) { CurrentText = StateNOTextLines[CurrentTextLine]; CurrentTextLine++; } else { CurrentState = "A"; CurrentTextLine = 3; CloseDialog();  }
+                break;
+            case "B":
+                if (CurrentTextLine < 3) { CurrentText = StateBTextLines[CurrentTextLine]; CurrentTextLine++; } else { CurrentTextLine = 2;  CloseDialog(); }
+                break;
+            case "C":
+                if (CurrentTextLine < 2) { CurrentText = StateCTextLines[CurrentTextLine]; CurrentTextLine++; } else { CloseDialog(); }
+                break;
+            case "DF":
+                if (CurrentTextLine < 1) { CurrentText = StateDFTextLines[CurrentTextLine]; CurrentTextLine++; } else { CloseDialog(); }
+                break;
+            case "G":
+                if (CurrentTextLine < 4) { CurrentText = StateGTextLines[CurrentTextLine]; CurrentTextLine++; } else { CurrentTextLine = 3; CloseDialog(); }
+                break;
+            case "H":
+                if (CurrentTextLine < 4) { CurrentText = StateHTextLines[CurrentTextLine]; CurrentTextLine++; } else { CloseDialog(); }
+                break;
+            case "I":
+                if (CurrentTextLine < 3) { CurrentText = StateITextLines[CurrentTextLine]; CurrentTextLine++; } else { CurrentTextLine = 2; CloseDialog(); }
                 break;
         }
 
+  
 
-       
        DialogTextObject.text = CurrentText;
        CanInput = true;
-       Debug.Log("Dialog updated");
+      
     }
+
+
+
 
     public void CloseDialog()
     {
+        StopAnyAudio();
         DialogObject.SetActive(false);
         IsTalking = false;
+        CanInput = true;
     }
     public void OpenDialog()
     {
         DialogObject.SetActive(true);
-        IsTalking = true;
+        
     }
 
     public void OpenQuestion()
@@ -185,7 +262,7 @@ public class OscarManager : MonoBehaviour
     public void ChooseQuestion()
     {
         CurrentTextLine = 0;
-      
+        StopAnyAudio();
         if (QuestionOrder)
         {
             CurrentState = "YES";
@@ -204,10 +281,36 @@ public class OscarManager : MonoBehaviour
         CanInput = true;
     }
 
-    IEnumerable Talking()
+
+    public void PlayAudio(string AudioReferenceToPlay)
+    {
+        instance = FMODUnity.RuntimeManager.CreateInstance(AudioReferenceToPlay); //choose audio  
+        instance.start();
+        instance.release(); //release audio from memory       
+        Debug.Log("Audio Played");
+    }
+
+    IEnumerator WaitForAudioToEnd()
     {
 
-        yield return new WaitForSeconds(1.2f);
+        instance.getDescription(out FMOD.Studio.EventDescription Des);
+        Des.getLength(out int lengthMili);
+        int lenght = lengthMili / 1000;
+        Debug.Log(lenght);
+
+
+        Debug.Log("Waiting");
+        yield return new WaitForSeconds(lenght); //wait till end of audio
+        Debug.Log("Done Waiting");
+        if (IsTalking) { NextLine(); }//play next line
+        else { CloseDialog(); }
     }
+    public void StopAnyAudio()
+    {        
+        instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE); //stop current audio
+        StopCoroutine(RoutineToStop); //stop current timer
+        Debug.Log("Audio Stopped");
+    }
+
 
 }
