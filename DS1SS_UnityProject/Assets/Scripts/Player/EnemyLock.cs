073a86e4058a327949fdf1d907315de43a739925
DaraghFiorilla <daraghfiorillafpl@gmail.com>
2023-05-28 18:55:33 +0100

    Update EnemyLock.cs

--- DS1SS_UnityProject/Assets/Scripts/Player/EnemyLock.cs
+++ DS1SS_UnityProject/Assets/Scripts/Player/EnemyLock.cs
@@ -7,30 +7,32 @@ public class EnemyLock : MonoBehaviour
     [SerializeField] private GameObject[] enemies;
     [SerializeField] private Transform nearestEnemyPos;
     [SerializeField] private GameObject player;
-    private bool? enemyToRight = null;
-    private PlayerControls playerControls;
+    private bool enemyToRight;
+    private PlayerController playerController;
+    public bool running;
 
     private void Start()
     {
+        playerController = GetComponent<PlayerController>();
         player = GameObject.FindGameObjectWithTag("Player");
         enemies = GameObject.FindGameObjectsWithTag("Enemy"); // Assigns all enemy tagged objects into array
         nearestEnemyPos = player.transform;
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.J)) // Placeholder input, this needs to be set to left arrow on dpad
+        if (running) // Placeholder input, this needs to be set to left arrow on dpad
         {
             nearestEnemyPos = GetNearestEnemy(); // Finds nearest enemy every frame
             //nearestEnemyPos.gameObject.GetComponent<SpriteRenderer>().material.color = Color.red; // Turns enemy gameobject red for debugging
         }
 
-        if (nearestEnemyPos.transform.position.x > transform.position.x)
+        if (nearestEnemyPos.transform.position.x > transform.position.x) // Detects if enemy is to right of player
         {
             enemyToRight = true;
         }
         else
         {
-            enemyToRight = false;
+            enemyToRight = false; // Enemy is to left
         }
     }
 
